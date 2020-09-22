c0a92db15fbda265fd5021b0a6d8ab2c727f5f7d
Mauro <mauro.spinosi94@gmail.com>
2020-09-22 15:06:22 +0200

    esercizio quasi completo

--- js/menu.js
+++ js/menu.js
@@ -1,3 +1,49 @@
 $(document).ready(function(){
 
+  $( ".bottone" ).click(function() {
+
+    $("#list_film").html("");
+
+    var searchFilm = $(".barra").val();
+    
+    $.ajax(
+      {
+        "url" : "https://api.themoviedb.org/3/search/movie",
+        "data" : {
+          "api_key" : "c0328fab8702a24e23778bd6bd73ba4b",
+          "query" : searchFilm,
+          "language" : "it-IT",
+        },
+        "method": "GET",
+        "success" : function (data) {
+          renderFilm(data.results);
+        },
+        "error": function (errore) {
+          alert("Errore!");
+        }
+      }
+    );
+  });
+
 });
+
+var source = $("#film-template").html();
+var template = Handlebars.compile(source);
+
+function renderFilm(film) {
+  for (var i=0; i<film.length; i++ ){
+    var context = {
+      "title": film[i].title,
+      "original_title": film[i].original_title,
+      "language": film[i].original_language,
+      "vote": film[i].vote_average,
+    };
+
+    var html = template(context);
+    $("#list_film").append(html);
+  }
+}
+
+// function chiamataApi(si) {
+//
+// }
